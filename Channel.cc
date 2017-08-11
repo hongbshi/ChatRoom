e5ed725785c15e5625c536abe62446ed0ffc5b9f
helloni <1162722785@qq.com>
2017-08-11 20:10:07 +0800

    writeToSocket

--- Channel.cc
+++ Channel.cc
@@ -18,35 +18,36 @@ void Channel::handleEvent()
 {
 	printf("File: Channel.cc, Channel::handleEvent function, channel fd is %d.\n", fd_);
 	//
-	if(reEvent_ & EPOLLIN) printf("EPOLLIN\n");
-	if(reEvent_ & EPOLLPRI) printf("EPOLLPRI\n");
-	if(reEvent_ & EPOLLRDHUP) printf("EPOLLRDHUP\n");
-	if(reEvent_ & EPOLLOUT) printf("EPOLLOUT\n");
-	if(reEvent_ & EPOLLERR) printf("EPOLLERR\n");
-	if(reEvent_ & EPOLLHUP) printf("EPOLLHUP\n");
+	//if(reEvent_ & EPOLLIN) printf("EPOLLIN\n");
+	//if(reEvent_ & EPOLLPRI) printf("EPOLLPRI\n");
+	//if(reEvent_ & EPOLLRDHUP) printf("EPOLLRDHUP\n");
+	//if(reEvent_ & EPOLLOUT) printf("EPOLLOUT\n");
+	//if(reEvent_ & EPOLLERR) printf("EPOLLERR\n");
+	//if(reEvent_ & EPOLLHUP) printf("EPOLLHUP\n");
 	//
-	if (reEvent_ == kNoneEvent)	return;
-	if(!(reEvent_ & EPOLLIN) && (reEvent_ & EPOLLHUP) && closeCallback_){
+	int ev = reEvent_;
+	if (ev == kNoneEvent)	return;
+	if(!(ev & EPOLLIN) && (ev & EPOLLHUP) && closeCallback_){
 		printf("File: Channel.cc, Channel::handleEvent function, close event.\n");
 		closeCallback_();
 	}
-	if ((reEvent_ & EPOLLERR) && errorCallback_){
+	if ((ev & EPOLLERR) && errorCallback_){
 		printf("File: Channel.cc, Channel::handleEvent function, error event.\n");
 		errorCallback_();
 	}
-	if ((reEvent_ & EPOLLOUT) && writeCallback_){
+	if ((ev & EPOLLOUT) && writeCallback_){
 		printf("File: Channel.cc, Channel::handleEvent function, write event.\n");
 		writeCallback_();
 	}
-	printf("***********************************\n");
-	if(reEvent_ & EPOLLIN) printf("EPOLLIN\n");
-	if(reEvent_ & EPOLLPRI) printf("EPOLLPRI\n");
-	if(reEvent_ & EPOLLRDHUP) printf("EPOLLRDHUP\n");
-	if(reEvent_ & EPOLLOUT) printf("EPOLLOUT\n");
-	if(reEvent_ & EPOLLERR) printf("EPOLLERR\n");
-	if(reEvent_ & EPOLLHUP) printf("EPOLLHUP\n");
-	printf("***********************************\n");
-	if ((reEvent_ & (EPOLLIN | EPOLLPRI | EPOLLRDHUP)) && readCallback_){
+	//printf("***********************************\n");
+	//if(reEvent_ & EPOLLIN) printf("EPOLLIN\n");
+	//if(reEvent_ & EPOLLPRI) printf("EPOLLPRI\n");
+	//if(reEvent_ & EPOLLRDHUP) printf("EPOLLRDHUP\n");
+	//if(reEvent_ & EPOLLOUT) printf("EPOLLOUT\n");
+	//if(reEvent_ & EPOLLERR) printf("EPOLLERR\n");
+	//if(reEvent_ & EPOLLHUP) printf("EPOLLHUP\n");
+	//printf("***********************************\n");
+	if ((ev & (EPOLLIN | EPOLLPRI | EPOLLRDHUP)) && readCallback_){
 		printf("File: Channel.cc, Channel::handleEvent function, read event.\n");
 		readCallback_();
 	}
